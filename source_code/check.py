53684e7b8b21555bf5121ee2dd6204fde29dc7b8
Ajith Kumar Palanikumar <ajithkumar.p@blue.cloud>
2023-09-19 17:10:02 +0530

    the error code

---
+++ source_code/check.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+df=pd.read_csv('C:\\Users\\AjithKumarPalanikuma\\Documents\\Project_BCGPT\\datamart\\Car Data.csv')
+
+a = list(df.columns)
+print((a))
--- source_code/csv_upload_flask_code.py
+++ source_code/csv_upload_flask_code.py
@@ -6,7 +6,7 @@ import os
 
 import pandas as pd
 
-
+recieved_df=[]
 ALLOWED_EXTENSIONS = set(['csv'])
 
 def allowed_file(filename):
@@ -21,10 +21,10 @@ def home_page():
    return render_template('home_template.html')
 
 
-@app.route('/upload', methods=['GET', 'POST'])
-def upload():
-    df_head_html=None
-    if request.method == 'POST':
+@app.route('/upload', methods = ['POST'])
+def get_df():
+    global recieved_df
+    if request.method == "POST":
         file = request.files['csvfile']
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
@@ -36,19 +36,18 @@ def upload():
             # data.to_html(classes='table table-bordered',index=False)
             df_head=data.head(10)
             df_head_html=df_head.to_html(classes='table table-bordered',index=False)
-    return render_template('data_source.html',df_head_html=df_head_html)        
+            recieved_df = list(data.columns)
+        return render_template('data_source.html', df_head_html = df_head_html)
+        
+@app.route('/analysis', methods = ['GET'])
+def analysis():
+    global recieved_df
+    cols = recieved_df
+    return render_template('analysis.html', cols = cols)
 
-
-
-
-
-
-
-    # if csv_file:
-    #     return 'CSV file uploaded and processed successfully.'
-    # else:
-    #     return 'No CSV file selected.'      
 
 if __name__ == '__main__':
-
     app.run(debug=True, port=5000)
+
+
+
